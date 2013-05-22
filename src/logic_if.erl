9088fb576085f0abceda36e523c174be4fb04b41
jacy <johnsoncr.hong@gmail.com>
2013-05-23 00:27:33 +0800

    fix pasal tri-angle bug.

--- src/logic_if.erl
+++ src/logic_if.erl
@@ -32,7 +32,9 @@ Velocity = math:sqrt(2 * Gravity * Distance),
 Description = if
 Velocity == 0 -> 'stable';
 Velocity < 5 -> 'slow';
-(Velocity >= 5) and (Velocity < 10) -> 'moving'; (Velocity >= 10) and (Velocity < 20) -> 'fast'; Velocity >= 20 -> 'speedy'
+(Velocity >= 5) and (Velocity < 10) -> 'moving'; 
+(Velocity >= 10) and (Velocity < 20) -> 'fast'; 
+Velocity >= 20 -> 'speedy'
 end,
 if
 (Velocity > 40) -> io:format("Look out below!~n") ;
